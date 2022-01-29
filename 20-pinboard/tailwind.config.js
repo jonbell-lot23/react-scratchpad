d96a0dfb6f7cce5d3088c91a7187f4f409152e4d
Jon Bell <github@lot23.com>
2022-01-29 06:18:49 +0000

    Fixing tailwind

--- 20-pinboard/tailwind.config.js
+++ 20-pinboard/tailwind.config.js
@@ -1,20 +1,9 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    container: {
-      center: true,
-
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
